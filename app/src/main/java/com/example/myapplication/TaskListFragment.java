8e20bf444ac7d342aa3a20edf83cd4214d15abae
Dzaky Abdi Al Jabbar <dzaky@ui.ac.id>
2020-10-12 21:01:30 +0700

    Shared data between fragments using viewmodel

---
+++ app/src/main/java/com/example/myapplication/TaskListFragment.java
@@ -0,0 +1,55 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.databinding.FragmentTaskListBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskListFragment extends Fragment {
+    private FragmentTaskListBinding binding;
+    private DetailsFragment detailsFragment = new DetailsFragment();
+
+    public TaskListFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_task_list, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SharedViewModel viewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+        List<Item> list = new ArrayList<>();
+        list.add(new Item("Dzaky Abdi Al Jabbar", "Cirebon", "17 September 1999"));
+        list.add(new Item("Annisa Nabila Aliyah", "Cirebon", "13 Juni 2003"));
+        list.add(new Item("Reni Rochini", "Cirebon", "13 Maret 1974"));
+        list.add(new Item("Nuryono", "Cirebon", "02 Desember 1973"));
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(list);
+        binding.recyclerView.setAdapter(adapter);
+        adapter.setListener((v, position) -> {
+            viewModel.setSelected(adapter.getItemAt(position));
+            getParentFragmentManager().beginTransaction()
+                    .replace(R.id.container, detailsFragment)
+                    .addToBackStack(null)
+                    .commit();
+        });
+    }
+}
